243574f9779f33cb7d3edcb15f883b1e2ed63d6d
thomas <tmwbook@gmail.com>
2014-04-21 00:19:19 -0400

    working on using requests.delete()

--- TwitchApp/TwitchApp.py
+++ TwitchApp/TwitchApp.py
@@ -3,10 +3,16 @@ __author__ = "thomas"
 import requests
 import json
 
-requestParam = {}
 
-r = requests.get('https://api.twitch.tv/kraken/teams/entertainment')
 
-data = json.loads(r.content)
 
-print data
+requestParam = {'OAuth': appId}
+
+
+r = requests.delete('https://api.twitch.tv/kraken/users/failfixer89/follows/channels/shotbownetwork_official', params=requestParam)
+
+re = requests.get('https://api.twitch.tv/kraken/users/failfixer89/follows/channels')
+data = json.loads(re.content)
+
+for channel in data['follows']:
+    print channel['_links']
