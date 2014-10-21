c0b1ef4b35620389a9ac7df49e2b84df1370953b
Thomas White <tmwbook@gmail.com>
2014-10-21 17:47:00 -0400

    Now sorts the title and description for multiple seasons

--- AnimeEpisodeInfo/finder.py
+++ AnimeEpisodeInfo/finder.py
@@ -7,14 +7,45 @@ Wikipedia has it so its Wikipedia.org/wiki/List_of_name_of_anime_episodes
 
 en_website_prefix_to_url = "http://en.wikipedia.org/wiki/List_of_"
 
-def find_episode_table(web_page_source):
+
+def find_episode_tables(web_page_source):
     source = BeautifulSoup(web_page_source)
-    return source.find("div",attrs={"id":  "content"})
+    return source.findAll("table", attrs={"class":  "wikitable"})
+
+
+def get_all_episodes_in_season(episode_tables):
+    all_seasons = {}
+    i = 1
+    for _ in episode_tables:
+        all_seasons[i] = []
+        i += 1
+    i = 1
+    for season in episode_tables:
+        titles = season.findAll("td", attrs={"class": "summary"})
+        descriptions = season.findAll("td", attrs={"class": "description"})
+        num_of_episodes = len(titles)
+        for j in range(0, num_of_episodes):
+            episode = [titles[j]]
+            try:
+                description = descriptions[j]
+            except IndexError:
+                description = "N/A"
+            finally:
+                episode.append(description)
+            all_seasons[i].append(episode)
+        i += 1
+    return all_seasons
+
+
+def get_episode_info(season, episode_num):
+    pass
+
 
 def get_page_source(anime_name):
     formatted_anime_name = format_name(anime_name)
     target_url = en_website_prefix_to_url + formatted_anime_name + "_episodes"
     return requests.get(target_url).content
+
 
 def format_name(title):
     list_of_words_in_title = title.split(" ")
@@ -27,5 +58,5 @@ def format_name(title):
 
 if __name__ == "__main__":
     test_anime = raw_input()
-    page_response =  get_page_source(test_anime)    
-    print find_episode_table(page_response)
+    page_response = get_page_source(test_anime)
+    print get_all_episodes_in_season(find_episode_tables(page_response))#testing
